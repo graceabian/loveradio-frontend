8948c3f361bf0c1d151d81301cec96bc49d9a4cb
Grace Abian <graceabian10@gmail.com>
2022-05-01 02:14:19 +0800

    done with banner radio button

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,25 +1,15 @@
-//This component will used as the hero section of our page.
-//Responsive -> grid system
-import { Row, Col } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 
-//we will use default bootstrap utility classes to format the component
-
-//create a function that will describe the structure of the hero section.
-
-//'class' ->reserved keyword (HTML)
-//React/JSX elements -> 'className'
 export default function Banner() {
   return (
-    <Row className="p-5">
-      <Col>
-        <h1> INSERT HEADING HERE </h1>
-        <p className="my-4">INSERT DESC HERE</p>
-        <a className="btn btn-primary" href="/">
-          INSERT ACTION HERE
-        </a>
-      </Col>
-    </Row>
+    <>
+      <Form.Check type="radio" aria-label="radio 1">
+        WATCH
+      </Form.Check>
+
+      <Form.Check type="radio" aria-label="radio 1">
+        LISTEN
+      </Form.Check>
+    </>
   );
 }
-
-//expose the component
