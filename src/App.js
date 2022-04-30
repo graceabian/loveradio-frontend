e423105a5cafa1a542a832d662f56538b27e293a
Grace Abian <graceabian10@gmail.com>
2022-04-30 17:45:42 +0800

    prepared project in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Hello From React</h1>
+        <h1>Hello Manila Broadcasting Company</h1>
+        <h2>Hello Love Radio Manila</h2>
+        <h3>prepared projects in Reacts</h3>
+      </div>
     </div>
   );
 }
-
 export default App;
