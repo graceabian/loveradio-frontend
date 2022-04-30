df4d7b4d4bb3f5082ea461093f4af23a0a414c88
Grace Abian <graceabian10@gmail.com>
2022-05-01 00:29:38 +0800

    done creating with banner compoment

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 
 //import the navbar
 import AppNavBar from "./components/AppNavBar";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div className="App">
       <div>
         <AppNavBar />
+        <Banner />
         <h1>Hello Love Radio Manila</h1>
       </div>
     </div>
---
+++ src/components/Banner.js
@@ -0,0 +1,25 @@
+//This component will used as the hero section of our page.
+//Responsive -> grid system
+import { Row, Col } from "react-bootstrap";
+
+//we will use default bootstrap utility classes to format the component
+
+//create a function that will describe the structure of the hero section.
+
+//'class' ->reserved keyword (HTML)
+//React/JSX elements -> 'className'
+export default function Banner() {
+  return (
+    <Row className="p-5">
+      <Col>
+        <h1> INSERT HEADING HERE </h1>
+        <p className="my-4">INSERT DESC HERE</p>
+        <a className="btn btn-primary" href="/">
+          INSERT ACTION HERE
+        </a>
+      </Col>
+    </Row>
+  );
+}
+
+//expose the component
